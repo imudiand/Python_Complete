465d2e0154f5023a96e86ab36a647e86a0809b0c
Harshit Imudianda <himudianda@apple.com>
2015-09-23 15:38:53 -0700

    Print matrix in spiral order added

--- challenges/main.py
+++ challenges/main.py
@@ -41,9 +41,44 @@ def print_matrix_vert():
     print ','.join(map(str, results))
 
 
+def print_top_right(matrix, x1, y1, x2, y2):
+    for x in range(x1, x2+1):
+        print matrix[y1][x]
+    for y in range(y1+1, y2+1):
+        print matrix[y][x2]
+
+    if x2-x1 and y2-y1:
+        print_bottom_left(matrix, x1, y1+1, x2-1, y2)
+
+def print_bottom_left(matrix, x1, y1, x2, y2):
+    for x in range(x2, x1-1, -1):
+        print matrix[y2][x]
+    for y in range(y2-1, y1-1, -2):
+        print matrix[y][x1]
+
+    if x2-x1 and y2-y1:
+        print_top_right(matrix, x1+1, y1, x2, y2-1)
+
+def print_matrix_spiral():
+    '''
+        Challenge3: Print matrix in spiral order
+        0, 0, 0, 0, 0
+        0, 1, 2, 3, 4
+        0, 2, 4, 6, 8
+        0, 1, 2, 3, 4
+    '''
+    matrix = [[0, 0, 0, 0, 0], [0, 1, 2, 3, 4], [0, 2, 4, 6, 8], [0, 1, 2, 3, 4]]
+    COL = len(matrix[0])
+    ROW = len(matrix)
+    print_top_right(matrix, 0, 0, COL-1, ROW-1)
+
+
 def main():
+    '''
     print_matrix_horz()
     print_matrix_vert()
+    '''
+    print_matrix_spiral()
 
 if __name__ == "__main__":
     main()
